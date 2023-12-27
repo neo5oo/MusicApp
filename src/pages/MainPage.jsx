4563237d002ae6adfbc8088808ada8cbc0374c6a
ne5oo <106486790+neo5oo@users.noreply.github.com>
2023-12-27 18:54:43 +0300

    Реализация поиска треков

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,13 +1,40 @@
+import { useState } from "react";
 import tracksList from "../assets/tracksList";
 import style from "./mainPage.module.scss";
 import Track from "../components/Track";
+import { Input } from "@mui/material";
+
+const runSearch = (query) => {
+  if (!query) {
+    return tracksList;
+  }
+
+  const lowerCaseQuery = query.toLowerCase();
+
+  return tracksList.filter(
+    (track) =>
+      track.title.toLowerCase().includes(lowerCaseQuery) ||
+      track.artists.toLowerCase().includes(lowerCaseQuery)
+  );
+};
 
 const MainPage = () => {
+  const [tracks, setTracks] = useState(tracksList);
+
+  const handleChange = (event) => {
+    const foundTracks = runSearch(event.target.value);
+    setTracks(foundTracks);
+  };
+
   return (
     <div className={style.search}>
-      <>Поиск треков</>
+      <Input
+        className={style.search}
+        onChange={handleChange}
+        placeholder="Поиск треков"
+      />
       <div className={style.list}>
-        {tracksList.map((track) => (
+        {tracks.map((track) => (
           <Track key={track.id} {...track} />
         ))}
       </div>
